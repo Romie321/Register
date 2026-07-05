dbbbfc41e1344bd200b15cf17427e4a8e7c6fb01
Romie321 <raguilera321@gmail.com>
2026-07-04 20:54:32 -0700

    Add variable declarations for cash register elements in script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let cash = getElementById("cash");
+let displayCashDue = getElementById("change-due");
+let purchaseBtn = getElementById("purchase-btn");
+let displayCid = getElementById("cash-in-drawer");
+
 let currencyUnits = [
   ["PENNY", 1],
   ["NICKEL", 5],
@@ -7,7 +12,7 @@ let currencyUnits = [
   ["FIVE", 500],
   ["TEN", 1000],
   ["TWENTY", 2000],
-  ["ONE HUNDRED",10000] 
+  ["ONE HUNDRED", 10000],
 ];
 
 function checkCashRegister(price, cash, cid) {
@@ -15,19 +20,19 @@ function checkCashRegister(price, cash, cid) {
   let cashOnHand = {};
   let cashToGive = {};
 
-  cid.forEach(currencyunit => {
-    cashOnHand[currencyunit[0]] = Math.round(currencyunit[1] * 100)
-  })
-  
+  cid.forEach((currencyunit) => {
+    cashOnHand[currencyunit[0]] = Math.round(currencyunit[1] * 100);
+  });
+
   let index = currencyUnits.length - 1;
 
-  while(index >= 0 && change > 0) {
+  while (index >= 0 && change > 0) {
     let moneyName = currencyUnits[index][0];
     let moneyValue = currencyUnits[index][1];
-    
-    if(change >= moneyValue > 0 && cashOnHand[moneyName] > 0) {
+
+    if (change >= moneyValue > 0 && cashOnHand[moneyName] > 0) {
       cashToGive[moneyName] = 0;
-      while(cashOnHand[moneyName] > 0 && change >= moneyValue) {
+      while (cashOnHand[moneyName] > 0 && change >= moneyValue) {
         cashOnHand[moneyName] -= moneyValue;
         cashToGive[moneyName] += moneyValue;
         change -= moneyValue;
@@ -35,28 +40,28 @@ function checkCashRegister(price, cash, cid) {
     }
     index -= 1;
   }
-  if(change === 0) {
+  if (change === 0) {
     let isRegisterEmpty = true;
 
-    Object.keys(cashOnHand).forEach(moneyType => {
-      if(cashOnHand[moneyType] > 0) {
+    Object.keys(cashOnHand).forEach((moneyType) => {
+      if (cashOnHand[moneyType] > 0) {
         isRegisterEmpty = false;
       }
     });
 
-    if(isRegisterEmpty) {
-      return {status: "CLOSED", change: cid}
+    if (isRegisterEmpty) {
+      return { status: "CLOSED", change: cid };
     } else {
       let changeArray = [];
-    Object.keys(cashToGive).map(moneyType => {
-      if(cashToGive[moneyType] > 0) {
-        changeArray.push([moneyType, cashToGive[moneyType] / 100]);
-      }
-    });
-    return {status: "OPEN", change: changeArray}
+      Object.keys(cashToGive).map((moneyType) => {
+        if (cashToGive[moneyType] > 0) {
+          changeArray.push([moneyType, cashToGive[moneyType] / 100]);
+        }
+      });
+      return { status: "OPEN", change: changeArray };
     }
   }
-  return {status: "INSUFFICIENT_FUNDS", change: []}
+  return { status: "INSUFFICIENT_FUNDS", change: [] };
 }
 
-console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
+//console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
