a41f5a76866729ef8b7bb96863270475595f689f
Romie321 <raguilera321@gmail.com>
2026-07-05 22:10:21 -0700

    Add cash-in-drawer data structure to script.js

--- script.js
+++ script.js
@@ -3,6 +3,17 @@ let displayCashDue = getElementById("change-due");
 let purchaseBtn = getElementById("purchase-btn");
 let displayCid = getElementById("cash-in-drawer");
 let price = getElementById("price");
+let cid = [
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90],
+  ["FIVE", 55],
+  ["TEN", 20],
+  ["TWENTY", 60],
+  ["ONE HUNDRED", 100],
+];
 
 let currencyUnits = [
   ["PENNY", 1],
