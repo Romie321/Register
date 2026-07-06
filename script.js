4ca71fc5744c5e6ea2bb9a9558948ef535a10133
Romie321 <raguilera321@gmail.com>
2026-07-05 21:50:19 -0700

    Add event listeners for purchase button and cash input in script.js

--- script.js
+++ script.js
@@ -65,3 +65,13 @@ function checkCashRegister(price, cash, cid) {
 }
 
 //console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
+
+window.onload = displayCashInDrawer();
+
+purchaseBtn.addEventListener("click", checkCashRegister);
+
+cash.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    checkCashRegister();
+  }
+});
